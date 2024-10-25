181baf006a141da40c31ff71bccc898990188207
Samriddhi Mishra <66818459+Samriddhi0201@users.noreply.github.com>
2024-10-25 21:10:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -102,9 +102,10 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.use((req, res , next) =>{
+     res.locals.currUser = req.user;
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    res.locals.currUser = req.user;
+   
     //console.log( res.locals.success);
     next();
 })
